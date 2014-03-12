a24c23fca5d3b6b32a00aad63343e73d2e69cf89
stefan judis <stefanjudis@gmail.com>
2014-03-12 23:51:58 +0100

    clean up + nicer structure and html serving

--- app.js
+++ app.js
@@ -6,49 +6,131 @@ var gm          = require( 'gm' );
 var imageMagick = gm.subClass( { imageMagick: true } );
 
 var server = http.createServer( function ( req, res ) {
-  // filter out favicon request
-  if ( /url/.test( req.url ) ) {
-    var url  = req.url.match( /\/+\?+url=(.*)/ )[ 1 ];
-    var file = config.picturePath + '/' + url.replace( /(http:\/\/)/, '' ) + '.png';
+  var match = req.url.match( /\/width\/(\d+?)\/height\/(\d+?)\/url\/(.+?)(\?scale=(\d+)){0,1}$/ );
 
-    console.log( url );
-    webshot(
-      url,
-      file,
-      {
-        screenSize :{
-          width  : 1000,
-          height : 750
-        }
-      },
-      function( err ) {
-        if ( err ) {
-          console.log( 'ERRRRORRR:' );
-          console.log( err );
-          res.send( 500 );
+  if ( match !== null ) {
+    var options = {
+      width  : match[ 1 ],
+      height : match[ 2 ],
+      url    : match[ 3 ],
+      scale  : match[ 5 ] || 100
+    };
 
-          return;
-        }
+    serveImage( options, res );
 
-        console.log( 'START RESIZING IMAGE' );
-        imageMagick( file )
-          .resize( config.image.width, config.image.height )
-          .autoOrient()
-          .write( file, function ( err ) {
-            if ( !err ) {
-              console.log(' hooray! ');
-              var img = fs.readFileSync( file );
-
-              res.writeHead( 200, {'Content-Type': 'image/gif' } );
-              res.end( img, 'binary' );
-            } else {
-              console.log( err );
-            }
-          } );
-      }
-    );
+  } else {
+    serveWebsite( res );
   }
 } );
+
+
+/**
+ * General function to kick off image serving
+ * either generate images and serve it
+ * or serve already generated images
+ *
+ * @param  {Object} options options
+ * @param  {Object} res     response object
+ */
+function serveImage( options, res ) {
+  options.filePath = config.picturePath + '/' +
+                      options.width + '/' +
+                      options.height + '/' +
+                      options.scale + '/' +
+                      options.url + '.png';
+
+  fs.exists( options.filePath , function( exists ) {
+    if ( exists ) {
+      var img = fs.readFileSync( options.filePath );
+
+      console.log( 'CACHED SERVING:' + options.filePath );
+      res.writeHead( 200, {'Content-Type': 'image/gif' } );
+      res.end( img, 'binary' );
+
+    } else {
+      createImage( options, res );
+    }
+  } );
+}
+
+
+/**
+ * Take picture with phantomjs
+ *
+ * @param  {Object} options options
+ * @param  {Object} res     response object
+ */
+function createImage( options, res ) {
+  console.log( 'http://' + options.url );
+  webshot(
+    options.url,
+    options.filePath,
+    {
+      screenSize :{
+        width  : options.width,
+        height : options.height
+      }
+    },
+    function( err ) {
+      if ( err ) {
+        console.log( 'ERROR IN WEBSHOT:' );
+        console.log( err );
+        res.statusCode = 500;
+        res.end();
+
+        return;
+      }
+
+      resizeImage( options, res );
+    }
+  );
+}
+
+
+/**
+ * Resize image with desired dimension
+ *
+ * @param  {Object} options options
+ * @param  {Object} res     response object
+ */
+function resizeImage( options, res ) {
+  imageMagick( options.filePath )
+    .resize(
+      options.width * options.scale / 100 ,
+      options.height * options.scale / 100
+    )
+    .autoOrient()
+    .write( options.filePath, function ( err ) {
+      if ( !err ) {
+        var img = fs.readFileSync( options.filePath );
+
+        console.log( 'GENERATED SERVING:' + options.filePath );
+        res.writeHead( 200, {'Content-Type': 'image/gif' } );
+        res.end( img, 'binary' );
+      } else {
+        console.log( 'ERROR IN IMAGEMAGICK' );
+        console.log( err );
+      }
+    } );
+}
+
+
+/**
+ * Serve website
+ * @param  {Object} res response object
+ */
+function serveWebsite( res ) {
+  fs.readFile( './index.html', 'utf8', function( err, file ) {
+    if ( err ) {
+      console.log( err );
+      res.statusCode = 500;
+      res.end();
+    } else {
+      res.writeHead( 200, { 'Content-Type' : 'text/html; charset=utf-8' } );
+      res.end( file, 'binary' );
+    }
+  } );
+}
 
 
 server.listen( config.port );
